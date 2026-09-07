31f0784ced3f8d1e08b437e63fa4a8108626b018
frogssoldseparately <frogssoldseparately@gmail.com>
2026-09-07 15:22:27 -0400

    Better read Adpcm books

--- pkg/zbank/book.go
+++ pkg/zbank/book.go
@@ -7,21 +7,25 @@ import (
 type AdpcmBook struct {
 	Order       uint32
 	NPredictors uint32
-	// Book        *[]uint8
-	Book *[]uint16
+	Book        *[]uint16
 }
 
 func ReadBook(r *sreader.SimpleReader) *AdpcmBook {
 	orderV := Read[uint32](r)
 	npredV := Read[uint32](r)
 	bookV := []uint16{}
-	lastRead := uint16(0xFFFF)
-	lastLastRead := uint16(0xFFFF)
-	for lastRead != lastLastRead || lastRead != 0 {
-		lastLastRead = lastRead
-		lastRead = Read[uint16](r)
-		bookV = append(bookV, lastRead)
+	consecutiveZeroCount := 0
+	nonZeroSeen := false
+	for !nonZeroSeen || (len(bookV)-4)%8 != 0 || consecutiveZeroCount != 4 {
+		v := Read[uint16](r)
+		bookV = append(bookV, v)
+		if v == 0 {
+			consecutiveZeroCount++
+		} else {
+			consecutiveZeroCount = 0
+			nonZeroSeen = true
+		}
 	}
-	bookV = bookV[0 : len(bookV)-2]
+	bookV = bookV[0 : len(bookV)-4]
 	return &AdpcmBook{orderV, npredV, &bookV}
 }
